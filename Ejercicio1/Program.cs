eb313103e8aa77feea00143a2d5b529e6df32f8c
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 10:57:11 +0100

    ejer1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = 35;
+            int v2 = 12;
+            int v3 = 17;
+
+            int total = v1 + v2 + v3;
+
+            Console.WriteLine("la suma de {0}, {1} y {2} es :{3}", v1, v2, v3, total);
+        }
+    }
+}
