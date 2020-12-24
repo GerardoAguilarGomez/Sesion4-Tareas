eb950b322b641b2968bdb43a0e39abefc136f1fe
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 11:30:32 +0100

    ejer9

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 1; x <= 100; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine(x);
+
+                }
+
+            }
+        }
+    }
+}
