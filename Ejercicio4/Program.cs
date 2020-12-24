d15fd372ab157d09050a4ce34d46b37bf0a81c0a
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 11:04:02 +0100

    ejer4

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1 = Console.ReadLine();
+            string n2 = Console.ReadLine();
+
+            int num1 = Convert.ToInt32(n1);
+            int num2 = Convert.ToInt32(n2);
+
+            int mayor = Math.Max(num1, num2);
+
+            Console.WriteLine("El número mayor es el {0}", mayor);
+        }
+    }
+}
