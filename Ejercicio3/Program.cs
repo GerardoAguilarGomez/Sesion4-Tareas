1f2170e9667d15008c44fedddb5f14dca7aa8d12
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 11:02:10 +0100

    ejer3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = Console.ReadLine();
+            string edad = Console.ReadLine();
+            Console.WriteLine("Te llamas {0} y tienes {1} años", nombre, edad);
+        }
+    }
+}
