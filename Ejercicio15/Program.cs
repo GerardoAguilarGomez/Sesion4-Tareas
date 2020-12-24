96109f489677aafbffe3e6dfbe590c4615367bc3
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 12:11:37 +0100

    ejer15

---
+++ Ejercicio15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte un valor:");
+            string n1 = Console.ReadLine();
+            int num = Convert.ToInt32(n1);
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Es divisible entre 2");
+            }
+            else
+            {
+                Console.WriteLine("No es divisible entre 2");
+            }
+        }
+    }
+}
