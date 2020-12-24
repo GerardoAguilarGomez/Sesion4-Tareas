ab0e09e68ec36878a250956c89b2b51f81f8c6fd
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 10:59:34 +0100

    ejer2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = Console.ReadLine();
+            string ciudad = Console.ReadLine();
+
+            Console.WriteLine("Hola {0}, bienvenido a {1}", nombre, ciudad);
+        }
+    }
+}
+
