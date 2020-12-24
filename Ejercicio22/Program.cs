e8ba88dceb644bb00bfd45228d734002d54f2c27
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 13:53:44 +0100

    ejer22

---
+++ Ejercicio22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clave = "c3po";
+            int intento = 0;
+
+            while (intento < 3)
+            {
+                Console.WriteLine("Inserte la contraseña: ");
+                string c = Console.ReadLine();
+
+                if (clave == c)
+                {
+                    Console.WriteLine("Enhorabuena");
+                    break;
+                }
+                else
+                {
+                    intento++;
+                }
+
+
+            }
+        }
+    }
+}
