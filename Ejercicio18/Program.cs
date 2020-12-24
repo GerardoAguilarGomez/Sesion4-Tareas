5b8e77d1e44baa1fbe29f34e5388dca08c07ed83
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 13:17:18 +0100

    ejer18

---
+++ Ejercicio18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 1; x <= 100; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
