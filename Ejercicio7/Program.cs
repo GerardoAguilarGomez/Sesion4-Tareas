37aace3660ad2f652c4ba52cff677db0be752128
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 11:24:50 +0100

    Ejer7

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 1; x <= 100; x++)
+            {
+
+                Console.WriteLine(x);
+
+            }
+        }
+    }
+}
