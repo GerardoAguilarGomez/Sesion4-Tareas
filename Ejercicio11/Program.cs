742fabef42bb810e8d7eede776588abb16fee1cd
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-24 11:43:38 +0100

    ejer11

---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 3;
+            int n2 = 3;
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("El número {0} es mayor que el {1}", n1, n2);
+            }
+            else if (n2>n1)
+            {
+                Console.WriteLine("El número {0} es mayor que el {1}", n2, n1);
+
+            }
+            else
+            {
+
+                Console.WriteLine("Los números son iguales");
+            }
+        }
+    }
+}
